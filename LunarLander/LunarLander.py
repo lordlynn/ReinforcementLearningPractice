3826963d520736a52895f74c68cb42b790a5a29a
Zac Lynn <zaclynn26@gmail.com>
2024-06-04 01:04:23 -0400

    Worked on LunarLaner and trained it

--- LunarLander/LunarLander.py
+++ LunarLander/LunarLander.py
@@ -1,22 +1,26 @@
+import tensorflow as tf
 import keras
 from keras import layers
 
+import keras.backend
 import keras.optimizers
 import numpy as np
 
 
 import gymnasium as gym
-
+import time
 
 
 class replayBuffer(object):
     def __init__(self, maxSize, inputShape, nActions, discrete=False):
         self.memSize = maxSize
         self.discrete = discrete
+
+        # What is the use of this??
+        dtype = np.int8 if self.discrete else np.float32
+
         self.stateMemory = np.zeros((self.memSize, inputShape))
         self.newStateMemory = np.zeros((self.memSize, inputShape))
-
-        dtype = np.int8 if self.discrete else np.float32
 
         self.actionMemory = np.zeros((self.memSize, nActions))
         self.rewardMemory = np.zeros(self.memSize)
@@ -27,23 +31,25 @@ class replayBuffer(object):
 
     def store_transition(self, state, action, reward, newState, done):
         # First available memory
-        index = self.memPtr % self.memSize
+        if (self.memPtr >= self.memSize):
+            self.memPtr = 0
+            print("**Replay buffer pointer rollover")
 
-        self.stateMemory[index] = state
-        self.newStateMemory[index] = newState
+        self.stateMemory[self.memPtr ] = state
+        self.newStateMemory[self.memPtr ] = newState
 
-        self.rewardMemory[index] = reward
+        self.rewardMemory[self.memPtr ] = reward
 
         # Should be false when episode is over, and true when episode is still going 
-        self.terminalMemory[index] = 1 - int(done)
+        self.terminalMemory[self.memPtr ] = 1 - int(done)
 
         # If discrete we one hot encode
         if self.discrete:
             actions = np.zeros(self.actionMemory.shape[1])
             actions[action] = 1.0
-            self.actionMemory[index] = actions
+            self.actionMemory[self.memPtr ] = actions
         else:
-            self.actionMemory[index] = action
+            self.actionMemory[self.memPtr ] = action
 
         self.memPtr += 1
 
@@ -65,25 +71,37 @@ class Agent(object):
     def __init__(self, alpha, gamma, nActions, epsilon, batchSize, inputDims, epsilonDec=0.996, epsilonEnd=0.01, memSize=1000000, fname='dqn_model.h5'):
         self.actionsSpace = [i for i in range(nActions)]
 
-        self.alpha = alpha
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilonDec = epsilonDec
-        self.epsilonMin = epsilonEnd
-        self.batchSize = batchSize
-        self.modelFile = fname
+        self.alpha = alpha              # Learning Rate
+        self.gamma = gamma              # Discount Factor
+        self.epsilon = epsilon          # Exploration rate
+        self.epsilonDec = epsilonDec    # Decay rate of epsilon
+        self.epsilonMin = epsilonEnd    # Minimum value of epsilon
+        self.batchSize = batchSize      # Training batch size
+        self.modelFile = fname          # File name to store/load model
 
         self.memory = replayBuffer(memSize, inputDims, nActions, discrete=True)
 
-        self.q_eval = self.build_network(alpha, nActions, inputDims, 256, 256)
+        self.q_eval = self.build_network(alpha, nActions, inputDims)
 
-    def build_network(self, learningRate, nActions, input_dims, fc1_dims, fc2_dims):
-        model = keras.Sequential([layers.Dense(fc1_dims, input_shape=(input_dims,)),
+    def build_network(self, learningRate, nActions, input_dims):
+        # inputs = layers.Input(shape=(input_dims,))
+
+        # HL1 = layers.Dense(64, activation='relu') (inputs)
+        # HL2 = layers.Dense(256, activation='relu') (HL1)
+        # HL3 = layers.Dense(128, activation='relu') (HL2)
+        
+        # outputs = layers.Dense(nActions, activation='softmax') (HL3)
+        
+        # model = keras.Model(inputs=inputs, outputs=outputs)
+
+        model = keras.Sequential([layers.Dense(256, input_shape=(input_dims,)),
                                 layers.Activation('relu'),
-                                layers.Dense(fc2_dims),
+                                layers.Dense(256),
                                 layers.Activation('relu'),
                                 layers.Dense(nActions)])
         
+       
+
         model.compile(optimizer=keras.optimizers.Adam(learning_rate=learningRate), loss='mse')
 
         return model
@@ -116,9 +134,12 @@ class Agent(object):
         action_indices = np.dot(action, action_values)
         action_indices = np.array(action_indices, dtype=np.int32)
 
-        q_eval = self.q_eval.predict(state, verbose=0)
-        q_next = self.q_eval.predict(newState, verbose=0)
+        q_eval = self.q_eval(state)
+        q_next = self.q_eval(newState)
 
+        q_eval = np.array(q_eval)
+        q_next = np.array(q_next)
+        
         q_target = q_eval.copy()
 
         batch_index = np.arange(self.batchSize, dtype=np.int32)
@@ -126,26 +147,32 @@ class Agent(object):
         # Current reward + best possible action from next state - used for loss
         q_target[batch_index, action_indices] = reward + self.gamma * np.max(q_next, axis=1) * done
 
-        next = self.q_eval.fit(state, q_target, verbose=0)
+        self.q_eval.fit(state, q_target, verbose=0)
+        
 
-        self.epsilon = self.epsilon * self.epsilonDec
+        if (self.epsilon > self.epsilonMin):
+            self.epsilon = self.epsilon * self.epsilonDec
 
-        self.epsilon = self.epsilonMin if (self.epsilon < self.epsilonMin) else self.epsilon
+        
 
     def save_model(self):
         self.q_eval.save(self.modelFile)
+        pass
 
     def load_model(self):
         self.q_eval = keras.models.load_model(self.modelFile)
+        pass
         
 
 
 
 
-env = gym.make("LunarLander-v2", render_mode="human", max_episode_steps=500)
+env = gym.make("LunarLander-v2")
 
+# Potentialy make the memSize 100k instead of 10k. See how this goes first
+agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, inputDims=8, nActions=4, memSize=100000, batchSize=64, epsilonEnd=0.010)
 
-agent = Agent(gamma=0.99, epsilon=1.0, alpha=0.0005, inputDims=8, nActions=4, memSize=1000000, batchSize=64, epsilonEnd=0.015)
+# agent.load_model()
 
 scores = []
 eps_history = []
@@ -156,25 +183,26 @@ for i in range(n_games):
     observation, info = env.reset()
 
     while not done:
+
         action = agent.choose_action(observation)
         newObservation, reward, done, truncated, info = env.step(action)
 
         if (truncated):
             done = True
-            reward -= 100
 
+    
         score += reward
         agent.remember(observation, action, reward, newObservation, done)
 
         observation = newObservation
-        agent.learn()
 
-        
+        agent.learn()
+        keras.backend.clear_session()
     
-    eps_history.append(agent.epsilon)
+
     scores.append(score)
 
-    avg_scores = np.mean(scores[max(0, i-100):i+1])
+    avg_scores = np.mean(scores[max(0, i-20):i+1])
 
     print(f"Episode {i}\tScore {score:.2f}\tAverage Score {avg_scores:.2f}")
 
